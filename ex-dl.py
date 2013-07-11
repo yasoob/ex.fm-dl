fc2ffad3977433d1cf0b2e22a157ef0c8128d62a
M.Yasoob Khalid <yasoob.khld@gmail.com>
2013-07-11 15:14:53 +0500

    now it works on windows as well.

--- ex-dl.py
+++ ex-dl.py
@@ -1,12 +1,6 @@
 import urllib2
 import json
 import sys
-import subprocess
-import os
-
-entry = """This program uses wget for downloading the song so right now it
-is available only for linux and mac and not for windows. However 
-I am working on making a windows version as well. :)\n"""
 
 def search(query, num):
 	query = query.replace(" ","%20")
@@ -29,10 +23,65 @@ def search(query, num):
 	    count += 1
 	return result
 
+def convertSize(n, format='%(value).1f %(symbol)s', symbols='customary'):
+    """
+    Convert n bytes into a human readable string based on format.
+    symbols can be either "customary", "customary_ext", "iec" or "iec_ext",
+    see: http://goo.gl/kTQMs
+    """
+    SYMBOLS = {
+    'customary'     : ('B', 'K', 'Mb', 'G', 'T', 'P', 'E', 'Z', 'Y'),
+    'customary_ext' : ('byte', 'kilo', 'mega', 'giga', 'tera', 'peta', 'exa',
+                       'zetta', 'iotta'),
+    'iec'           : ('Bi', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi', 'Yi'),
+    'iec_ext'       : ('byte', 'kibi', 'mebi', 'gibi', 'tebi', 'pebi', 'exbi',
+                       'zebi', 'yobi'),
+    }
+    n = int(n)
+    if n < 0:
+        raise ValueError("n < 0")
+    symbols = SYMBOLS[symbols]
+    prefix = {}
+    for i, s in enumerate(symbols[1:]):
+        prefix[s] = 1 << (i+1)*10
+    for symbol in reversed(symbols[1:]):
+        if n >= prefix[symbol]:
+            value = float(n) / prefix[symbol]
+            return format % locals()
+    return format % dict(symbol=symbols[0], value=n)
+
+def download(fileurl,file_name):
+    ##Initiate download
+    u = urllib2.urlopen(fileurl)
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading %s (%s)" %(file_name, convertSize(file_size))
+     
+    #Calculate downloaded filesize
+    file_size_dl = 0
+    block_size = 8192
+     
+    #Download loop
+    while True:
+        buffer = u.read(block_size)
+        if not buffer:
+            break
+     
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%s [%3.2f%%]" % (convertSize(file_size_dl), file_size_dl * 100. / file_size)
+        status = status + chr(8)*(len(status)+1)
+        #print status
+        sys.stdout.write("\r        %s" % status)
+        sys.stdout.flush()
+     
+    #Download done. Close file stream
+    f.close()
+
 if __name__ == '__main__':
 	try:
 		print "Ex.fm song downloader made by M.Yasoob <yasoob.khld@gmail.com>"
-		print entry
 		query = raw_input("Which song or artist do you want to search for?   ")
 		num = raw_input("How many results do you want to retrieve ?   ")
 		print ""
@@ -42,8 +91,7 @@ if __name__ == '__main__':
 		title = results[int(songId)]['title']
 		music = urllib2.urlopen(url)
 		print "Downloading the file..."
-		cmd = 'wget -O "%s.mp3" "%s"' % ( title, url) #Run wget to download the song
-		p = subprocess.Popen(cmd, shell=True)
+		download(url,title+".mp3")
 	except KeyboardInterrupt:
 		print "\nProgram was closed by the user\n"
 		sys.exit()
